64a65b67b4b7e5ce7ee1adeafe13d1204aa8e53b
Chris Bremmer <bremmer.dev@gmail.com>
2020-04-08 13:43:45 -0700

    conert project to JSX, and React-specific ESLint config

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-}
+const Pet = props => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
+};
+
+export default Pet;
